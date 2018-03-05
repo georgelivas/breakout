55d0d215eb7e29bf50994fbe0f1e49ef4efa9b31
Unknown <giorgos.livas.99@gmail.com>
2018-03-05 23:41:38 +0000

    remove imports

--- src/com/company/Model.java
+++ src/com/company/Model.java
@@ -2,10 +2,6 @@ package com.company;
 
 import java.util.*;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Model extends Observable {
     private static final int B = 6;                 // Border offset
